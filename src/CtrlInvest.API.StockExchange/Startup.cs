18241177cca057a0719b7fccc5ad115a9a78b97b
Juliano Cesar Pestili <juliano.pestili@outlook.com>
2022-06-09 19:37:03 -0300

    Enable Https in prod

--- src/CtrlInvest.API.StockExchange/Startup.cs
+++ src/CtrlInvest.API.StockExchange/Startup.cs
@@ -170,15 +170,14 @@ namespace CtrlInvest.API.StockExchange
             app.UseDeveloperExceptionPage();
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", $"CtrlInvest.API.StockExchange v1"));
-            
-            //else
-            //{
-            //    //The default HSTS value is 30 days. 
-            //    //You may want to change this for production
-            //    //scenarios, see https://aka.ms/aspnetcore-hsts.
-            //    app.UseHsts();
-            //}
-            //app.UseHttpsRedirection();
+
+            if (env.EnvironmentName == "Production")
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+                app.UseHttpsRedirection();
+            }
+                       
 
             app.UseHealthChecks("/health", new HealthCheckOptions
             {
